c98aa297a834eaffcb1618412911c1bd0c631b78
arunprakash12345 <arunprakash74119@gmail.com>
2025-11-24 08:57:14 +0530

    Definition and Learned scope chaining and the Closure with basic example and added 2 more examples.

--- Js/scope.js
+++ Js/scope.js
@@ -1,5 +1,15 @@
-//1 
+// Scope in JavaScript refers to the accessibility of variables and functions in different parts of the code. 
+// There are two main types of scope: 
+// global scope and local scope. 
+// Variables declared outside any function have global scope, 
+// while those declared within a function have local scope.
+//Scope is directly dependent on the lexical environment where the variable is defined.
+// Lexical environment refers to the local memory along with the reference to its lexical environment of its parent.
+// Lexical - Hierarchy of the code.
+//Scope chains - Each lexical environment has a link to its outer lexical environment.
 
+
+//1 
 function a()
 {
     console.log("Variable b inside function a:", b);
@@ -40,10 +50,28 @@ function example3()
     }
     inner3();
 }
-console.log("Accessing variable d outside its scope:", d);
+// console.log("Accessing variable d outside its scope:", d);
 example3();
 
 // output:
 // Accessing variable d outside its scope: ReferenceError: d is not defined
 // Reason: The variable 'd' is declared within the function 'example3' and is not accessible outside of it. When trying to log 'd' outside the function, it results in a ReferenceError because 'd' is not defined in that scope. 
 
+
+
+
+//4
+function example2()
+{
+    var c=20;
+    function inner2()
+    {
+        console.log("Inner function accessing outer variable c:", c);
+    }
+    inner2();
+}
+example2();
+// output:
+// Inner function accessing outer variable c: 20
+// Reason: The variable 'c' is declared within the function 'example2' and is accessible within the inner function 'inner2'. When 'inner2' is called, it logs the value of 'c', which is 20.
+// Closure: The inner function 'inner2' forms a closure, capturing the variable 'c' from its outer lexical environment ('example2'). This allows 'inner2' to access 'c' even after 'example2' has finished executing.
