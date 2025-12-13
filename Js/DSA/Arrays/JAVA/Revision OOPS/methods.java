66e13b3780fa244f95f53770aa87d03e745f6585
arunprakash12345 <arunprakash74119@gmail.com>
2025-12-13 20:44:52 +0530

    Interfaces and Abstract classes.

---
+++ Js/DSA/Arrays/JAVA/Revision OOPS/methods.java
@@ -0,0 +1,7 @@
+public class methods implements Notification {
+    public void notify(String text){
+        System.out.println("Notified with Text msg.");
+    } 
+}
+
+
---
+++ Js/DSA/Arrays/JAVA/Revision OOPS/sms.java
@@ -0,0 +1,5 @@
+public class sms implements Notification{
+    public void notify(String text){
+        System.out.println("Notified with Text msg.");
+    } 
+}
