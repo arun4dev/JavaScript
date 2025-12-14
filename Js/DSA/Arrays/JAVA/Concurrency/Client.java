847cd706b40c740b4068c45533c73ff13a36c230
arunprakash12345 <arunprakash74119@gmail.com>
2025-12-14 17:07:00 +0530

    Add example done.

--- Js/DSA/Arrays/JAVA/Concurrency/Client.java
+++ Js/DSA/Arrays/JAVA/Concurrency/Client.java
@@ -1,35 +1,49 @@
+import java.util.*;
 public class Client {
     public static void main(String[] args) {
 
-        System.out.println(
-            Thread.currentThread().getName() + " I am the main class"
-        );
+        // System.out.println(
+        //     Thread.currentThread().getName() + " I am the main class"
+        // );
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
 
-        Adder adder = new Adder();
-        Subtractor subtractor = new Subtractor();
+        Adder adder = new Adder(a,b);
+
+        // Subtractor subtractor = new Subtractor();
 
         Thread t1 = new Thread(adder);
-        Thread t2 = new Thread(subtractor);
+        // Thread t2 = new Thread(subtractor);
 
         t1.start();
-        t2.start();
+        // t2.start();
     }
 }
 
 class Adder implements Runnable {
+    int a;
+    int b;
+    Adder(int a,int b){
+        this.a=a;
+        this.b=b;
+    }
+    public void sum(int a,int b)
+    {
+        System.out.println(a+b);
+    }
     @Override
     public void run() {
-        System.out.println(
-            Thread.currentThread().getName() + " I am the Adder class"
-        );
+        sum(a,b);
+        // System.out.println(
+        //     Thread.currentThread().getName() + " I am the Adder class"
+        // );
     }
 }
 
 class Subtractor implements Runnable {
     @Override
     public void run() {
-        System.out.println(
-            Thread.currentThread().getName() + " I am the Subtractor class"
-        );
+        // System.out.println(Thread.currentThread().getName() + " I am the Subtractor class");
     }
 }
