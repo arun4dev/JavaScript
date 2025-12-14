851c4b4962d5103e23538bc27459d904f14e838d
arunprakash12345 <arunprakash74119@gmail.com>
2025-12-14 17:00:51 +0530

    Example 1 of Threads.

--- Js/DSA/Arrays/JAVA/Concurrency/Client.java
+++ Js/DSA/Arrays/JAVA/Concurrency/Client.java
@@ -1,11 +1,35 @@
-
-
 public class Client {
     public static void main(String[] args) {
-        HelloWorldPrinter h = new HelloWorldPrinter();
 
-        Thread t =new Thread(h);
-        t.start();
+        System.out.println(
+            Thread.currentThread().getName() + " I am the main class"
+        );
 
+        Adder adder = new Adder();
+        Subtractor subtractor = new Subtractor();
+
+        Thread t1 = new Thread(adder);
+        Thread t2 = new Thread(subtractor);
+
+        t1.start();
+        t2.start();
+    }
+}
+
+class Adder implements Runnable {
+    @Override
+    public void run() {
+        System.out.println(
+            Thread.currentThread().getName() + " I am the Adder class"
+        );
+    }
+}
+
+class Subtractor implements Runnable {
+    @Override
+    public void run() {
+        System.out.println(
+            Thread.currentThread().getName() + " I am the Subtractor class"
+        );
     }
 }
